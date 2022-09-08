6bcd28f846c2878bdf2d67ab61f6cfcefa57d583
phucdhh <phucdhh@galaxy.com.vn>
2022-09-08 22:04:06 +0700

    change db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,28 @@
 const { Sequelize } = require('sequelize');
 
+// const sequelize = new Sequelize({
+//     database: "dbufpmmmefosui",
+//     username: "bkineamgiumsna",
+//     password: "399f2382b1ab695983c8510c8eaa9ea24945559a9044363b2eedb2ffeb11a887",
+//     host: "ec2-34-235-198-25.compute-1.amazonaws.com",
+//     port: 5432,
+//     dialect: "postgres",
+//     define: {
+//         timestamps: false
+//     },
+//     dialectOptions: {
+//         // timezone: '+07:00',
+//         ssl: {
+//             require: true, // This will help you. But you will see nwe error
+//             rejectUnauthorized: false // This line will fix new error
+//         }
+//     },
+// });
 const sequelize = new Sequelize({
-    database: "dbufpmmmefosui",
-    username: "bkineamgiumsna",
-    password: "399f2382b1ab695983c8510c8eaa9ea24945559a9044363b2eedb2ffeb11a887",
-    host: "ec2-34-235-198-25.compute-1.amazonaws.com",
+    database: "dc9b19mjjcl1f4",
+    username: "fzspnkptxqzeyo",
+    password: "46613b9fe42356bdff01eaa4091c7ffb7e799530d28c8170f292451f97f8ccfc",
+    host: "ec2-3-225-110-188.compute-1.amazonaws.com",
     port: 5432,
     dialect: "postgres",
     define: {

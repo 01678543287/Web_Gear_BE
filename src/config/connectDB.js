0333fd38a17c664ea8f3245ebfe83d096cc323cb
phucdhh <phucdhh@galaxy.com.vn>
2022-09-09 10:55:37 +0700

    add localhost connect db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 // const sequelize = new Sequelize({
 //     database: "dbufpmmmefosui",
@@ -19,36 +19,53 @@ const { Sequelize } = require('sequelize');
 //     },
 // });
 const sequelize = new Sequelize({
-    database: "dc9b19mjjcl1f4",
-    username: "fzspnkptxqzeyo",
-    password: "46613b9fe42356bdff01eaa4091c7ffb7e799530d28c8170f292451f97f8ccfc",
-    host: "ec2-3-225-110-188.compute-1.amazonaws.com",
-    port: 5432,
-    dialect: "postgres",
-    define: {
-        timestamps: false
+  database: "dc9b19mjjcl1f4",
+  username: "fzspnkptxqzeyo",
+  password: "46613b9fe42356bdff01eaa4091c7ffb7e799530d28c8170f292451f97f8ccfc",
+  host: "ec2-3-225-110-188.compute-1.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  define: {
+    timestamps: false,
+  },
+  dialectOptions: {
+    // timezone: '+07:00',
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
     },
-    dialectOptions: {
-        // timezone: '+07:00',
-        ssl: {
-            require: true, // This will help you. But you will see nwe error
-            rejectUnauthorized: false // This line will fix new error
-        }
-    },
+  },
 });
 
+// const sequelize = new Sequelize({
+//     database: "gpgear",
+//     username: "postgres",
+//     password: "123",
+//     host: "localhost",
+//     port: 5432,
+//     dialect: "postgres",
+//     define: {
+//         timestamps: false
+//     },
+//     // dialectOptions: {
+//     //     // timezone: '+07:00',
+//     //     ssl: {
+//     //         require: true, // This will help you. But you will see nwe error
+//     //         rejectUnauthorized: false // This line will fix new error
+//     //     }
+//     // },
+// });
+
 let connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-
-
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 module.exports = {
-    connectDB,
-    sequelize
-}
+  connectDB,
+  sequelize,
+};
